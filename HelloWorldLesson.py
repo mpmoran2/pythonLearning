9f48e71f1e6714e429e4cb5e9c429c8bf4d76501
mpmoran2 <kamokumoran@gmail.com>
2021-02-12 11:23:01 -0800

    Single Line Code Intro

---
+++ HelloWorldLesson.py
@@ -0,0 +1,30 @@
+print('Hello World')
+# in the console, will get back Hello World
+
+200+300
+# in the console, will get 500
+
+200-300
+# in the console, will get -100
+
+75*32
+# in the console, will get 2400
+
+5*6(5+89)
+# in the console, will get an error. Must write it as follows:
+5*6*(5+89)
+# we need to multiply the result of 5*6 with the sum of 5+89
+
+# There are two ways to do division in python
+500/100
+# In the console, we get a float of 5.0 however, if we use
+500//100
+# in the console, we get 5
+
+#like many languages, we cannot divide by 0 and will get an error.
+
+#Float notes: if you use floats in your equation, value will be float
+6.0*3
+# value will come out as 18.0
+6*3
+# value will come out as 18
