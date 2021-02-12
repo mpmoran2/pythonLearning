7424c1b2abe5090907c89c1f06abbcb3fbf7b1be
mpmoran2 <kamokumoran@gmail.com>
2021-02-12 11:36:00 -0800

    Intro to Math Equations in Python

---
+++ Mathematical.py
@@ -0,0 +1,20 @@
+#intro to more math in python
+#exponent use x**x
+2**2 #this is 2 to the power of 2 and should give us 4 in the console
+
+2**3 #2 to the power of 3 should give is 8
+
+#square root uses x**(1/2)
+49**(1/2) #the square root is 7. Since 1/2 is .5 our answer will be a float 7.0
+
+#remainders x%x
+7/3 #this gets us 2.3333333333333335 if we want to see the remainder
+7%3 #this will give us 1 and is useful to see if it is odd or even
+
+3984%2 #if the answe is 0 it is even, if 1 it is odd
+
+#exercise from Instructor
+7%(5//2)
+# 1
+#logic behind this: 5//2 will be 2 since x//x gets rid of float
+# % will give us a remainder and 7%2 is 1 as there is a remainder of 1 so answer will be 1
