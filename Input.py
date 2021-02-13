bd082454c2dd6bb900109207fc7c1ee921a9545a
mpmoran2 <kamokumoran@gmail.com>
2021-02-13 12:20:18 -0800

    Input from user lesson

---
+++ Input.py
@@ -0,0 +1,16 @@
+# how to accept input from a user
+
+input('What is your zodiac?')
+# This will prompt a >? where user can input
+# later will learn how to store the input to allow for answers
+
+# How to print 2 seperate lines
+print('Hello Miss welcome home')
+# Say we want to have it print as:
+# Hello Miss
+# Welcome Home
+# We use \n
+print('Hello Miss \n Welcome Home')
+# OR
+print('Hello Miss \nWelcome Home')
+# the only differance is how it looks in the console.
