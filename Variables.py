08aecc06aa6a4b3f378ab31882fe19e9cf5201a2
mpmoran2 <kamokumoran@gmail.com>
2021-02-17 15:32:18 -0800

    Variables Lesson

---
+++ Variables.py
@@ -0,0 +1,22 @@
+# Variables in Python
+
+a = 10
+# a is our variable and we are assigning 10 to it
+# if we use:
+print(a)
+# the console will return 10 to us in the console
+
+# again we will create another variable
+b = 40
+
+# when we use the console and ask to print(b), we will get 40
+# moving to more complex variables
+c = a + b
+
+# when we use print(c) we will get 50 in the console
+
+# this will not save is just using the console and will only save for use during the session
+# can also store strings as long as you remember to use '' or ""
+
+# Removing variable data
+del variable
