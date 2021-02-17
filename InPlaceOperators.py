b32fe9ae646e204237de6c5143ab1f9de6b54791
mpmoran2 <kamokumoran@gmail.com>
2021-02-17 15:41:13 -0800

    In Place Opperators Lesson

---
+++ InPlaceOperators.py
@@ -0,0 +1,17 @@
+age = 39
+
+# to increase value of 1
+
+age + 1
+# this will increase the value but the variable data remains at 39 so we have to write it as such
+age = age + 1
+
+age += 1
+# this will tell that it takes the current variable age and up it by 1. can be used for birthday updates
+# this can be used for subtraction and multiplication
+
+age -= 4
+age *= 2
+
+b="hello"
+b+=" world"
