eff118a51475db7b7a7e6e42c4990424b496a754
mpmoran2 <kamokumoran@gmail.com>
2021-02-18 13:20:22 -0800

    Operations On Strings Lesson

---
+++ OperationsOnString.py
@@ -0,0 +1,16 @@
+#concatination
+
+print("hello" + "world") # helloworld
+print("hello" + " " + "world") # hello world
+
+print("5" + "5") # This will give us 55 not 10
+
+print(5+5) # This gives us 10
+
+print("hello" + "I" + "am" + "happy") # helloIamhappy
+
+print("hello" + 5) # gets an error. concatination only works with strings not a string and intiger
+
+print("Beep"*3) # gives us BeepBeepBeep we can multiply a string with an integer
+
+print("Beep"*"3") # this gives us an error as we cannot multiply strings
