9f3648777a55f57dc5f722506aaf83a9b6b62988
mpmoran2 <kamokumoran@gmail.com>
2021-02-21 14:15:25 -0800

    If Statement Lesson

---
+++ IfStatement.py
@@ -0,0 +1,8 @@
+age = int(input('Please enter your age.'))
+# age is saves as in integer that the user inputs
+
+if age>= 18:
+    print('You are legally an adult') # MUST indent
+else:
+    print('A child')
+
