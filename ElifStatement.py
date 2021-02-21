f1b3127b10c44708ecb7a4cf2bea1ece88feb1ef
mpmoran2 <kamokumoran@gmail.com>
2021-02-21 14:22:04 -0800

    Elif Statement Lesson

---
+++ ElifStatement.py
@@ -0,0 +1,14 @@
+# Grading System to explore Elif Statement
+
+marks = int(input('Enter your marks, please >'))
+
+if marks>= 90:
+    print('Grade: A')
+elif marks>= 80:
+    print('Grade: B')
+elif marks>= 70:
+    print('Grade: C')
+elif marks>= 60:
+    print('Grade: D')
+elif marks< 60:
+    print('Sorry, you be failing')
