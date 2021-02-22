d4dfba24cf9f83a92cbb5fd943275837fb977d39
mpmoran2 <kamokumoran@gmail.com>
2021-02-22 14:48:58 -0800

    List Lesson

---
+++ Lists.py
@@ -0,0 +1,20 @@
+# Intro to lists
+# Similar to Arrays
+names =["Pilahi", "Juan", "Nathan", "Samantha", "Hannah"]
+print(names)
+
+# To find out index
+print(names[1]) #like arrays, index starts at 0 so 1=Juan while Pilahi is at 0
+
+# Numerical List
+numbers =[2,3,4,9]
+print(numbers)
+
+#empty lists
+empty =[]
+
+# List Operations
+
+# List Functions
+
+# Range Function
