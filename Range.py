53c387ba55652f478779a0010ea2c6069d04c9cd
mpmoran2 <kamokumoran@gmail.com>
2021-02-22 15:13:21 -0800

    Range Functions Lesson

---
+++ Range.py
@@ -0,0 +1,8 @@
+numbers = list(range(10)) # creates a list with 10 numbers 0-9
+print(numbers)
+
+nums = list(range(3,8)) # creates a list starting at 3 and ends at 8. Will show 3,4,5,6,7
+print(nums)
+
+nnummssss = list(range(1,30,3)) # creates a list that starts at 1, ends before 30 and in intervals of 3
+print(nnummssss)
