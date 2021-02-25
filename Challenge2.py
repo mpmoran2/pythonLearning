5746cdeb21f8227982ff50d3d05181df1028f4c1
mpmoran2 <kamokumoran@gmail.com>
2021-02-25 15:45:35 -0800

    Coding Challenge2

---
+++ Challenge2.py
@@ -0,0 +1,12 @@
+# Create a list of fav food items (Min 5)
+favs = ["Ramen", "Sushi", "Poke", "Sashimi", "Fruits", "Edamame"]
+# List out the 3rd element of the list
+print(favs[3])
+# Add additional Item to the current list and display list
+favs.append("Corn Chowder")
+print(favs)
+# Insert an element names "Tacos" at the 3rd index position of the list and print out the list elements
+favs.insert(3,"Tacos")
+print(favs)
+
+
