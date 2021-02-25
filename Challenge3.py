44376da9c0d67e889f8fe9d69724ba0bdc1e73ef
mpmoran2 <kamokumoran@gmail.com>
2021-02-25 15:51:30 -0800

    Coding Challenge3

---
+++ Challenge3.py
@@ -0,0 +1,10 @@
+# Use a for loop and rance function to print "I am a programmer" 5 times
+for x in range(1,6):
+    print("I am a programmer")
+
+# Create a functions that displays out the square values of numbers 1-9
+def square():
+    for x in range(1,10):
+        print(x*x)
+
+square()
