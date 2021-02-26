a7ecf823a2f493cb0658cbf17526e16d7ab92983
mpmoran2 <kamokumoran@gmail.com>
2021-02-25 16:00:47 -0800

    Passing in arguments to functions Lesson

---
+++ PassingArguments.py
@@ -0,0 +1,13 @@
+# Passing Arguments to functions
+
+def add(a,b):
+    print(a+b)
+
+add(2,4)
+
+def funkySheep(a,b,c):
+    print(a + " " + b)
+    print("Imma" + " " + c)
+    print("I said" + " " + a + " " + b + " " + "Imma" + " " + c )
+
+funkySheep("Bleep", "Beep", "Sheep")
