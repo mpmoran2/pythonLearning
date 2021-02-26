00b120a4c0ceba9f74ee98b50d8bac5ad8fec02c
mpmoran2 <kamokumoran@gmail.com>
2021-02-25 16:09:22 -0800

    Using function Returns Lesson

---
+++ FunctionReturn.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    c = a + b # adding 2 arguments, saved in variable c
+    return c # we will want to return the sum only
+
+result = add(8,43) # we need to accept the value of c to the variable result and can then print it
+print(result) # this should give is the answer of 3 + 43
+
+
+# example
+def duck(a):
+    b = a*3
+    return b
+result = duck("quack ")
+print("And the Duck goes")
+print(result)
