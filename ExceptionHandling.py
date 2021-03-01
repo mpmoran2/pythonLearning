d6e0ce47e02303117b14c5e40f32533dd14b8e75
mpmoran2 <kamokumoran@gmail.com>
2021-03-01 14:31:38 -0800

    Error Handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,11 @@
+# Try Statment/Try Block
+try:
+    a = 20
+    b = 0
+    print(a/b)
+# Needs an Except Block
+except ZeroDivisionError:
+    print('There is a Divide By Zero Error!')
+
+# This will not make our code crash because we have handled the exception error
+
