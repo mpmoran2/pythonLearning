15a10a7c56eba23c4b129408040a799b0798cd07
mpmoran2 <kamokumoran@gmail.com>
2021-03-02 11:38:11 -0800

    Adding data to a .txt file

--- Lessons/FileHandling.py
+++ Lessons/FileHandling.py
@@ -12,9 +12,23 @@
 
 
 # Reading Data from a File
-file = open("filehandling.txt", 'r') # Mode is read
-content = file.read() # This will read the content from the file and save into the variable content.
-# Parameter here would be reading 10 bytes of data from the file
-# If you only need to read a particular line, use .readline() function
+# file = open("filehandling.txt", 'r') # Mode is read
+# content = file.read() # This will read the content from the file and save into the variable content.
+# # Parameter here would be reading 10 bytes of data from the file
+# # If you only need to read a particular line, use .readline() function
+# print(content)
+# file.close()
+
+# Adding data to the file
+file = open("filehandling.txt", 'w')
+file.write("Hire me please")
+file.close()
+
+file = open("filehandling.txt", 'r')
+content = file.read()
 print(content)
 file.close()
+
+# If we need to add more we must use the Append mode, other wise anything we try to add in write mode will just over ride the previous data in the .txt file
+
+# Appending data to the file
