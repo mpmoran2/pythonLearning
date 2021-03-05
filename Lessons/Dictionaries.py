1609fbc47ab7bcf1e2b5b6740e385824871bde90
mpmoran2 <kamokumoran@gmail.com>
2021-03-04 17:42:17 -0800

    Dictionaries

---
+++ Lessons/Dictionaries.py
@@ -0,0 +1,15 @@
+# Dictionaries are data structures used to map arbitrary keys to values.
+# They help you capture data and store it in the code
+# Taking that data, manipulating it, and giving a certian output
+# Ex: You want to find the meaning of cloud. You would look it up in the dictionary and get the meaning of the word
+# In pairs. Key and value
+
+# EX Code: Saving the ages of people in code
+people = {"Pilahi":29, "Morgan":28} # Dictionaries are in curly braces and come in key:value pairs
+print(people["Morgan"]) # To look up the value of a key you need to state the dictionary name[key]
+# We are telling python to map the key we give to the value that is associated to it
+
+# fun example
+cute = {"Sheep":"Yes", "Otter":"Yes", "Mole Rat":"Hell No"}
+print(cute["Mole Rat"])
+
