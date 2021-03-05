950449d35659da1f75068f0a239bae7ba329960e
mpmoran2 <kamokumoran@gmail.com>
2021-03-04 17:54:39 -0800

    Tuples

---
+++ Lessons/Tuples.py
@@ -0,0 +1,8 @@
+# Tuples
+# Data structures similar to lists but are imutable(Cant change the values stored)
+
+fruits = ("Peach", "Mango", "Guava", "Passionfruit", "Pineapple") # will still run without the paranthesis
+print(fruits)
+print(fruits[2])
+
+# if you try to reassign a value, it will give an error
