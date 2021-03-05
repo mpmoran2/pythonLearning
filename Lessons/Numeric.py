7a613c4490f21c1fae70b2e2d5a82fea18878369
mpmoran2 <kamokumoran@gmail.com>
2021-03-04 18:46:28 -0800

    Numeric Functions

---
+++ Lessons/Numeric.py
@@ -0,0 +1,10 @@
+# Numerical functions (Most basic ones)
+
+# min() finding the minimum possible value
+print(min(1,2,3,4,5))
+
+# max() opposite of min()
+print(max(1,2,3,4,5))
+
+# abs() absolute value: The whole value (always going to be positive)
+print(abs(-203))
