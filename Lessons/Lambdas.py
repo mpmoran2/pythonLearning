583ad0b70f15ec8ba1f22903cc93a49adb4f2b74
mpmoran2 <kamokumoran@gmail.com>
2021-03-08 14:50:34 -0800

    Filter in lambdas

--- Lessons/Lambdas.py
+++ Lessons/Lambdas.py
@@ -13,3 +13,15 @@ print(result)
 
 # Do not even need variable
 print((lambda x: x ** 2)(79))
+
+
+
+# Filters: functions used with lambdas to filter out a list
+
+numbs = [1,3,4,5,7,2,9,11,13]
+
+result = list(filter(lambda x: x%2 == 0, numbs))
+# takes in the lambda and the list as arguments as you want to perform the lambda function on the list
+
+print(result)
+
