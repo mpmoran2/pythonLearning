f3eacbade9674a078db95f09cfa1104175087738
mpmoran2 <kamokumoran@gmail.com>
2021-03-09 12:43:56 -0800

    Instance Attributes & Constructor

--- Lessons/Class.py
+++ Lessons/Class.py
@@ -5,9 +5,6 @@
 class Sheep: # a template or "mould" of our objects
     # add class attributes
     herd = 50
-    color = "White"
-    wool = "Floofy"
-    horns = 2
 
 sheepy = Sheep() # this created the object
 sheepies = Sheep()
@@ -15,5 +12,21 @@ sheepies = Sheep()
 print(sheepy.herd)
 print(sheepies.herd)
 
-# changing attribute by using instance Attributes
 
+# Instance Attributes
+class Dog:
+    good_baby = 2
+    # constructor = a method to define the instance attribute
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+
+# dogo = Dog()
+# print(dogo) # This gives us an error because we didn't give arguments of Name or Color
+dogo = Dog("Missy","Black")
+print(dogo.name)
+print(dogo.color)
+
+dogogo = Dog("Tahoe","Mixed")
+print(dogogo.name)
+print(dogogo.color)
