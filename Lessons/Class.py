d79632f198d6da121129529b07c6bde804ff2b54
mpmoran2 <kamokumoran@gmail.com>
2021-03-09 12:04:36 -0800

    Creating classes and assiging attributes

--- Lessons/Class.py
+++ Lessons/Class.py
@@ -2,3 +2,18 @@
     # how we make Objects
 #Class Methods
     # a way to manipulate the attribute of your object
+class Sheep: # a template or "mould" of our objects
+    # add class attributes
+    herd = 50
+    color = "White"
+    wool = "Floofy"
+    horns = 2
+
+sheepy = Sheep() # this created the object
+sheepies = Sheep()
+
+print(sheepy.herd)
+print(sheepies.herd)
+
+# changing attribute by using instance Attributes
+
