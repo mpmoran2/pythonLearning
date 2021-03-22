b5685375fe8305cffa15ae31c90767732e7c7bea
mpmoran2 <kamokumoran@gmail.com>
2021-03-22 09:04:08 -0700

    Inheritance

--- Lessons/Function vs OOP.py
+++ Lessons/Function vs OOP.py
@@ -1,13 +1,33 @@
 # Function Based
-def get_stats:
-    name = input("Enter Name")
-    age = input("Enter Age")
-    print(name)
-    print(age)
-
-get_stats()
+# def get_stats():
+#     name = input("Enter Name")
+#     age = input("Enter Age")
+#     print(name)
+#     print(age)
+#
+# get_stats()
 
 # OOP
+# ALT VERSION OOP
+# # class Person:
+# #     def __init__(self,name,age):
+# #         self.name = name
+# #         self.age = age
+# #
+# #     def get_info(self,name,age):
+# #         self.name = name
+# #         self.age = age
+# #
+# #     def give_info(self):
+# #         print(self.name)
+# #         print(self.age)
+# #
+# # person1 = Person("", "")
+# # name = input("Enter Name")
+# # age = input("Enter Age")
+# # person1.get_info(name,age)
+# # person1.give_info()
+
 class Person:
     def __init__(self,name,age):
         self.name = name
@@ -20,27 +40,18 @@ class Person:
     def give_info(self):
         print(self.name)
         print(self.age)
-
-person1 = Person("", "")
-person1.get_info()
-person1.give_info()
-
-# ALT VERSION OOP
-# class Person:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
-#
-#     def get_info(self,name,age):
-#         self.name = name
-#         self.age = age
-#
-#     def give_info(self):
-#         print(self.name)
-#         print(self.age)
 #
 # person1 = Person("", "")
-# name = input("Enter Name")
-# age = input("Enter Age")
-# person1.get_info(name,age)
+# person1.get_info()
 # person1.give_info()
+
+# Inheritance: One class can inherit the methods and properties of another class
+class Student(Person): # We add the class we want to inherit from
+
+    def grade(self):
+        print("They are doing well.")
+
+a = Student("","")
+a.get_info()
+a.grade()
+
