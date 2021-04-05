864ac0125a2177e72cd635ecc664b4c4a45f012e
mpmoran2 <kamokumoran@gmail.com>
2021-04-05 11:42:13 -0700

    Regular Expressions dot metacharacter

--- Lessons/Regular Expressions.py
+++ Lessons/Regular Expressions.py
@@ -17,12 +17,12 @@ else:
     print('No match has been found')
 # Search & Find All
 if re.search(pattern,"baconeggseggsbacon"): # on the otherhand, search gives us a match found
-    print('Match found')
+    print('Match has now been found')
 else:
     print('No match has been found')
 
 if re.findall(pattern,"baconeggseggsbacon"): # findall will find all patterns in a string.
-    print('Match found')
+    print('Matches have been found')
 else:
     print('No match has been found')
     # OR
@@ -35,8 +35,13 @@ pattern = r"Megan" # We pass this into the subfunction
 newstring = re.sub(pattern,"Pilahi",string) # subfunction.
 
 print(newstring)
-# Dot Metacharacter
 
+# Dot Metacharacter
+    # Metacharacters allow us to make our regular expressions more powerful
+pattern = r"gr.y" # the . between gr and y means that there can be any characters in between like grey, gray, etc.
+
+if re.match(pattern, "grpy"):
+    print('A match has been found')
 # Caret & Dollar metacharacter
 
 # Character Class
