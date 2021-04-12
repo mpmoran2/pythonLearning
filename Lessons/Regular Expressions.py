13fed75511d1b6c55b4ad2e77c766fad17d12bbe
mpmoran2 <kamokumoran@gmail.com>
2021-04-12 15:55:55 -0700

    Regular Expressions Star Metacharacter

--- Lessons/Regular Expressions.py
+++ Lessons/Regular Expressions.py
@@ -36,8 +36,24 @@ newstring = re.sub(pattern,"Pilahi",string) # subfunction.
 
 print(newstring)
 
+# Character Class
+    # provide us a way to match up a specific set of characters
+    # ex: how streets are named or college campus buildings.
+    # a naming convention basically
+    #
+pattern = r"[A-Z][A-Z][0-9]" # this possition should be any letter a-z or 0-9
+if re.search(pattern, "MP7"):
+    print("Match has been found.")
+else:
+    print("No match has been found.")
+
+# Group
+
+
+
 # METACHARACTERS
     # Metacharacters allow us to make our regular expressions more powerful
+
 # Dot Metacharacter
 pattern = r"gr.y" # the . between gr and y means that there can be any characters in between like grey, gray, etc.
 
@@ -54,18 +70,16 @@ pattern = r"^gr.y$" # Unlike with search/findall, this specifies the string MUST
 if re.match(pattern, "gray"):
     print('Matched!')
 
-# Character Class
-    # provide us a way to match up a specific set of characters
-    # ex: how streets are named or college campus buildings.
-    # a naming convention basically
-    # great for sign up and verifications
-pattern = r"[A-Z][A-Z][0-9]" # this possition should be any letter a-z or 0-9
-if re.search(pattern, "MP7"):
-    print("Match has been found.")
-else:
-    print("No match has been found.")
-
-
 # Star Metacharacter
+    # allows 0+ repititions of previous things (class, group, or singular character)
+pattern = r"eggy(bacon)*" # this signifies that any thing right before the star can be repeated 0+ times in a string
 
-# Group
+if re.match(pattern, "egg"): #this will be accepted along with "eggsbacon" however, not with just "bacon" because eggs is always part of the string but not bacon
+    print("Matchy match!")
+else:
+    print("Nada")
+
+
+
+
+
