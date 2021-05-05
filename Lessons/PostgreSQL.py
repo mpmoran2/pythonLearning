a92bcc29bc2d70ee109460ddc76a17b11afe492d
mpmoran2 <kamokumoran@gmail.com>
2021-05-05 16:30:35 -0700

    Databases

---
+++ Lessons/PostgreSQL.py
@@ -0,0 +1,15 @@
+#  How Code Execution works
+# Input => Python Code => Output => Console
+    # Usually when console is closed and code stops running, information is lost
+    # If we want to save the input/output even if code stops, we save it to Databases.
+
+# Why would we want to store our data?
+    # By storing data, we are about to perform these operations:
+        # Can read the data when we want without running our code
+            # Ex: Banking Application
+        # Can update/modify data
+            # Ex: Updating addresses/name/contact info
+        # Can delete data
+
+# Data is stored in the form of tables
+    # EX: ID, Name, Address, Phone Number. These are attributes of a person.
